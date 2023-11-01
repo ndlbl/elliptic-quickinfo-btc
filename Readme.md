c590ecc6845540206e121eb4df4450b2ad1906a1
Indelible <heff@ndlbl.com>
2023-11-01 21:00:48 +0000

    Initial commit, repo created for elliptic demo #ticket-number
    add new files to project as part of time based assesment.
    
    5 mins planning
    
    1hr 25 maybe 1 hr 30 mins actual coding
    
    10 mins admin on repos and such (add linting/formatting etc)
    moving forarwd I would switch to an actual logging agent and provide some fallback for the blockcypher API

---
+++ Readme.md
@@ -0,0 +1,18 @@
+## Elliptic Quick Info
+
+Run a quick scan on a bitcoin address and receive back balance and XCP asset information.
+
+```
+$ node index.js %ADDRESS%
+
+```
+
+
+some example address to run might be:
+
+* 1GQhaWqejcGJ4GhQar7SjcCfadxvf5DNBD   early artist
+* 1Q5RMGBu65zURuVvGMuF57BkW1Z7KyvLMH   big collector
+* 1J8aLankHbmAHojseGRmWRZoTZKiNVi5GQ  random XCP user
+* 1NwCmg8gZW7KykrA7mX16zugNZZuckPg5o random XCP user
+
+Seems to suffer from rate limiting from blockcypher.
---
+++ index.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+// use proper logging instead of console, and use axios error.
+
+const resources = [{ url: 'https://api.blockcypher.com/v1/btc/main/addrs/{:address}' }, { url: 'https://xchain.io/api/address/{:address}' }, { url: 'https://xchain.io/api/balances/{:address}' }]
+
+if (process.argv.length === 2) {
+  console.error('Expected at least one adress as argument!')
+  process.exit(1)
+}
+
+process.argv.slice(2).forEach(function (val, index, array) {
+  Promise.all(resources.map((resource) => {
+    return axios.get(resource.url.replace('{:address}', process.argv[2 + index]))
+  })).then(function (responses) {
+    responses.forEach((response, i) => {
+      // a quick way to force behaviour for certain endpoints data shapes, when typescript could refer them as types here.
+      if (i === 0) {
+        console.log(`The address ${process.argv[2 + index]}, and holds a bitcoin balance of  ${response.data.final_balance} satoshis or ${response.data.final_balance / 100000000}, with ${response.data.unconfirmed_n_tx} unconfirmed transactions`)
+      }
+
+      if (i === 2) {
+        if (response.data.assets.held !== 0) {
+          console.log(`also holding  ${response.data.assets.held} Counterparty (XCP) Assets`)
+        }
+      }
+      if (i === 2) {
+        const HighValues = []
+        if (response.data.data.length > 0) {
+          response.data.data.forEach(asset => {
+            if ((asset.estimated_value.usd / asset.quantity > 1000) && asset.quantity > 1) {
+              HighValues.push((`${asset.asset}, $${(asset.estimated_value.usd / asset.quantity).toFixed(2)} each, ${asset.quantity} of. `))
+            }
+          })
+          if (HighValues.length) {
+            console.log(`${HighValues.length} High Value Items Found`)
+            console.log(`${HighValues.join('\n')}`)
+          }
+        }
+      }
+    })
+  }).catch((reason) => {
+    console.log(`Error:${reason.code}`)
+    console.log('you probably ran out of API requests on blockcypher, try again in a few mins')
+  })
+})
